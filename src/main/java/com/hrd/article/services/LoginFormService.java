9e581ed4ce4673b7d00826367fae5838cd231dc8
phearun008 <rathphearun123@gmail.com>
2015-11-27 15:28:43 +0700

    User Login Service

--- src/main/java/com/hrd/article/services/LoginFormService.java
+++ src/main/java/com/hrd/article/services/LoginFormService.java
@@ -3,9 +3,7 @@ package com.hrd.article.services;
 import com.hrd.article.entities.UserDTO;
 
 public interface LoginFormService {
-	
-	public boolean isLogin(String name,String pwd);
-	
+
 	public UserDTO getUser(String name,String pwd);
 	
 }
